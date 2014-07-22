fc0a8496fb9e185ec75f8d27c37fd8eceef0ebb1
Ignacio Suay <ignacio.suay@wwarn.org>
2014-07-22 16:29:45 +0100

    Create a couple of test: - testWriteIndex: this test will create an index - testQueryLucene: this test will query the index

--- src/test/java/com/ignaciosuay/writer/LuceneIndexWriterTest.java
+++ src/test/java/com/ignaciosuay/writer/LuceneIndexWriterTest.java
@@ -1,20 +1,70 @@
 package com.ignaciosuay.writer;
 
 
+import junit.framework.Assert;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by suay on 5/13/14.
  */
 public class LuceneIndexWriterTest {
 
+    static final String INDEX_PATH = "indexDir";
+    static final String JSON_FILE_PATH = "/test.json";
+
     @Test
-    public void testLuceneConfig(){
+    public void testWriteIndex(){
         try {
-            LuceneIndexWriter lw = new LuceneIndexWriter("indexDir", "/test.json");
+            LuceneIndexWriter lw = new LuceneIndexWriter(INDEX_PATH, JSON_FILE_PATH);
             lw.createIndex();
+
+            //Check the index has been created successfully
+            Directory indexDirectory = FSDirectory.open(new File(INDEX_PATH));
+            IndexReader indexReader = DirectoryReader.open(indexDirectory);
+
+            int numDocs = indexReader.numDocs();
+            assertEquals(numDocs, 3);
+
+            for ( int i = 0; i < numDocs; i++)
+            {
+                Document document = indexReader.document( i);
+                System.out.println( "d=" +document);
+            }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void testQueryLucene() throws IOException, ParseException {
+        Directory indexDirectory = FSDirectory.open(new File(INDEX_PATH));
+        IndexReader indexReader = DirectoryReader.open(indexDirectory);
+        final IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+        Term t = new Term("name", "id2");
+        Query query = new TermQuery(t);
+
+        TopDocs topDocs = indexSearcher.search(query, 10);
+        assertEquals(1, topDocs.totalHits);
+
     }
 }
